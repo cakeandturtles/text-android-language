febfdf9f238e5dda8d89714dcbcae3a1c3975429
jake trower <jakeonaut@gmail.com>
2015-09-18 16:19:58 -0500

    adding JavaScript examples

---
+++ examples/foo.js
@@ -0,0 +1,10 @@
+function food(){
+  console.log("yum, yum!");
+}
+
+function foo(x){
+  console.log(x);
+}
+
+food();
+foo(12);
---
+++ examples/variables.js
@@ -0,0 +1,5 @@
+//allow emojis as variables
+//does javascript allow??
+❤ = 3;
+😀 = 5;
+console.log(😀+❤);
